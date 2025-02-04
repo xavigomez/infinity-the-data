8138f144278689435994455366f518b8efbaaa68
xavigomez <codegoonie@gmail.com>
2025-02-04 01:50:47 +0100

    remove post component from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center">
-        {hello.greeting}
+        work in progress
       </main>
     </HydrateClient>
   );
