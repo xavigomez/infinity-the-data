44df181c131d09d1b5fdb549eadbae80547cf1d6
xavigomez <codegoonie@gmail.com>
2025-02-24 22:56:49 +0100

    add loaders and see al tournaments to last tourna

--- src/app/_components/latest-tournaments.tsx
+++ src/app/_components/latest-tournaments.tsx
@@ -1,7 +1,15 @@
 "use client";
 
-import { Calendar, Target, Users } from "lucide-react";
+import {
+  ArrowRight,
+  Calendar,
+  CircleGauge,
+  FileBadge,
+  Target,
+  Users,
+} from "lucide-react";
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
 import {
   Card,
   CardContent,
@@ -10,50 +18,72 @@ import {
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
+import { Skeleton } from "~/components/ui/skeleton";
 import { api } from "~/trpc/react";
 
 export function LatestTournaments() {
   const { data: tournaments, isLoading: isLoadingTournaments } =
     api.tournaments.getAllTournaments.useQuery();
 
-  if (isLoadingTournaments) return <div>Loading...</div>;
-  if (!tournaments) return <div>No tournaments found</div>;
+  if (!isLoadingTournaments && !tournaments) return;
   return (
     <article className="space-y-4">
-      <h2 className="font-title text-3xl font-bold text-primary">
-        Latest Tournaments
-      </h2>
-      <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {tournaments.map((tournament) => (
-          <li key={tournament.id}>
-            <Link href={`/tournament/${tournament.slug}`}>
-              <Card>
-                <CardHeader className="pb-2">
-                  <CardTitle>{tournament.name}</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <ul className="flex gap-2 text-sm [&>li]:flex [&>li]:items-center [&>li]:gap-1">
-                    <li>
-                      <Calendar className="size-4 text-secondary" />
-                      <span>
-                        {tournament.tournamentDate.toLocaleDateString()}
-                      </span>
-                    </li>
-                    <li>
-                      <Users className="size-4 text-secondary" />
-                      <span>{tournament.numberOfPlayers} players</span>
-                    </li>
-                    <li>
-                      <Target className="size-4 text-secondary" />
-                      <span>{tournament.numberOfRounds} rounds</span>
-                    </li>
-                  </ul>
-                </CardContent>
-              </Card>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div className="flex items-center justify-between">
+        <h2 className="font-title text-3xl font-bold text-primary">
+          Latest Tournaments
+        </h2>
+        <Link href="/tournaments">
+          <Button variant="text" size="text">
+            <span>View all tournaments</span>
+            <ArrowRight className="size-4" />
+          </Button>
+        </Link>
+      </div>
+      {isLoadingTournaments && (
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          <Skeleton className="h-24" />
+          <Skeleton className="h-24" />
+          <Skeleton className="h-24" />
+        </div>
+      )}
+      {!isLoadingTournaments && !tournaments && <div>No tournaments found</div>}
+      {!isLoadingTournaments && tournaments && (
+        <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {tournaments.map((tournament) => (
+            <li key={tournament.id}>
+              <Link href={`/tournament/${tournament.slug}`}>
+                <Card>
+                  <CardHeader className="pb-2">
+                    <CardTitle>{tournament.name}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <ul className="grid grid-cols-2 gap-2 text-sm [&>li]:flex [&>li]:items-center [&>li]:gap-1">
+                      <li>
+                        <Users className="size-4 text-secondary" />
+                        <span>{tournament.numberOfPlayers} players</span>
+                      </li>
+                      <li>
+                        <Target className="size-4 text-secondary" />
+                        <span>{tournament.numberOfRounds} rounds</span>
+                      </li>
+                      <li>
+                        <Calendar className="size-4 text-secondary" />
+                        <span>
+                          {tournament.tournamentDate.toLocaleDateString()}
+                        </span>
+                      </li>
+                      <li>
+                        <FileBadge className="size-4 text-secondary" />
+                        <span>ITS {tournament.its}</span>
+                      </li>
+                    </ul>
+                  </CardContent>
+                </Card>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </article>
   );
 }
